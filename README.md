9b63abdb410b8e579eff0e112391cc65a408c0fc
Frank Klingebiel <frank_kl@t-online.de>
2019-05-02 23:07:50 +0200

    First implementation commit. Supporting devicelist and bandwith usage calls.

---
+++ README.md
@@ -0,0 +1,48 @@
+Fritz Node
+======================================
+Uses node.js to access the fritz box with a minimal set of external dependencies. 
+ 
+
+API calls provided
+------------------
+
+- getDeviceList
+
+Returns active and inactive network devices known to the box.
+
+- getBandwithUsage
+
+Returns the current download and upload rate.
+
+Example:
+```javascript
+run = async()=>{
+    let con = await module.exports.fritz({
+        password : 'secret'
+    });
+    let usage = await con.getBandwithUsage();
+    console.log(JSON.stringify(usage,' ','  '));
+};
+run();
+```
+
+Produces
+```json
+{
+  "downMax": 24331000,
+  "downCurrent": 27240,
+  "upMax": 4899000,
+  "upCurrent": 6693
+}
+```
+
+
+Environment variables supported
+-------------------------------
+
+| Name       | Description | Default
+|------------|-------------|---------
+| FRITZ_HOST | Fritz box host name or IP. | fritz.box
+| FRITZ_USER | Fritz user name. | admin
+| FRITZ_PASSWORD | Fritz box password. 
+| LOG_LEVEL | Console log level. Supported are TRACE,DBEUG,INFO,WARN, ERROR | INFO
---
+++ index.js
@@ -0,0 +1,100 @@
+const http = require('./lib/http.js');
+const log = require('./lib/log.js');
+
+module.exports.fritz = async (opt) => {
+    let context = {
+        user : opt.user || process.env.FRITZ_USER || 'admin',
+        password : opt.password || process.env.FRITZ_PASSWORD,
+        host : opt.host || process.env.FRITZ_HOST || 'fritz.box'
+    }
+    if( !context.password ) {
+        throw new Error('A password must be set.')
+    }
+    var createPath = (action,params) => {
+        let url = 'http://'+context.host;
+        url+='/'+action;
+        if( params || context.sid ) {
+            url += '?';
+            for( key in params ) {
+                if(url.slice(-1)!='?') url += '&';
+                url += (key +'='+ encodeURIComponent(params[key]));
+            }
+            if( context.sid ) {
+                if(url.slice(-1)!='?') url += '&';
+                url += 'sid='+context.sid;
+            }
+        }
+        return url;
+    }
+
+    log.info("Login "+context.user+"@"+context.host+ " ... ");
+
+    // Get a challenge
+    let loginResult = await http.getXml(createPath('login_sid.lua'));
+    let challenge = loginResult.SessionInfo.Challenge;
+    
+    log.debug("Challenge "+challenge);
+
+    // Resolve the challenge
+    let buffer = Buffer.from(challenge + '-' + context.password, 'UTF-16LE')
+    let challengeResolved = challenge + '-' + require('crypto').createHash('md5').update(buffer).digest('hex')
+    let challengeResponse = await http.getXml(
+        createPath('/login_sid.lua', { username : context.user, response: challengeResolved })
+    )
+
+    // Get the session ID.
+    context.sid = challengeResponse.SessionInfo.SID;
+  
+    // Check SID
+    if (context.sid === '0000000000000000') {
+      throw new Error('Login to Fritz!Box at '+host+' with user '+user+' failed. Invalid login?')
+    }
+
+    log.debug("Session ID "+context.sid);
+
+    log.info("Login "+context.user+"@"+context.host+ " ... done.");
+    
+    return {
+        sid : context.sid,
+        getDeviceList : async ()=>{
+            let data = await http.postForm(createPath('data.lua'), {
+                sid : context.sid,
+                page : 'netDev'
+            });
+            let dataObj = JSON.parse(data);
+            const createDevice = (raw, active) => {
+                return {
+                    mac : raw.mac,
+                    name : raw.name,
+                    active : active,
+                    ip : raw.ipv4 || raw.ipv6,
+                    port : raw.port
+                }
+            }
+            const devices = [];
+            dataObj.data.active.forEach( (d) => {
+              devices.push( createDevice(d, true) );
+            });
+            dataObj.data.passive.forEach( (d) => {
+                devices.push( createDevice(d, false) );
+              });
+              return devices;
+        },
+        getBandwithUsage: async() => {
+            let data = await http.get(createPath('/internet/inetstat_monitor.lua',
+                {   action:'get_graphic',
+                    xhr:1,
+                    myXhr:1,
+                    useajax:1
+                })
+            );
+            let dataObj = JSON.parse(data);
+            return {
+                downMax : dataObj[0].downstream,
+                downCurrent : dataObj[0].ds_bps_curr_max,
+                upMax : dataObj[0].upstream,
+                upCurrent : dataObj[0].us_bps_curr_max
+            }
+        } 
+    }
+}
